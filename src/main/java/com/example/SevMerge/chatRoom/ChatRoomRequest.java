d7b4633d952462c593b619064307c74bf3cb6326
san <san1425865@gamil.com>
2026-06-01 11:50:06 +0900

    feat: response, request 구현중

--- src/main/java/com/example/SevMerge/chatRoom/ChatRoomRequest.java
+++ src/main/java/com/example/SevMerge/chatRoom/ChatRoomRequest.java
@@ -1,4 +1,16 @@
 package com.example.SevMerge.chatRoom;
 
+import lombok.Data;
+
+
 public class ChatRoomRequest {
+    @Data
+    public static class ChatRoomDTO {
+        private Long projectId;
+
+        private Long clientId;
+
+        private String expertId;
+    }
+
 }
--- src/main/java/com/example/SevMerge/chatRoom/ChatRoomService.java
+++ src/main/java/com/example/SevMerge/chatRoom/ChatRoomService.java
@@ -1,4 +1,19 @@
 package com.example.SevMerge.chatRoom;
 
+import com.example.SevMerge.member.MemberRepository;
+import com.example.SevMerge.project.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberRepository memberRepository;
+    private final ProjectRepository projectRepository;
+
+    public ChatRoomResponse createChatRoom(ChatRoomRequest reqDTO) {
+
+    }
 }
