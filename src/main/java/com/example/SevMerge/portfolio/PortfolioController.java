a518c23f902369eb53c131d93ff561829794da01
dkswnsgus66 <dkswnsgus66@gmail.com>
2026-06-04 16:34:41 +0900

    portfolio파일업로드작업중

--- src/main/java/com/example/SevMerge/portfolio/PortfolioController.java
+++ src/main/java/com/example/SevMerge/portfolio/PortfolioController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -78,7 +79,11 @@ public class PortfolioController {
     @PostMapping("/portfolios/save")
     public String savePortfolios(PortfolioRequest.SaveDTO saveDTO) {
 
-        portfolioService.save(saveDTO);
+        try {
+            portfolioService.save(saveDTO);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         return "redirect:/portfolios";
     }
